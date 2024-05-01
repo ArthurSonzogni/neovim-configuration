1ee06783b4ef961e9dbd10aab9eae0a3e014613b
ArthurSonzogni <sonzogniarthur@gmail.com>
2024-05-01 11:41:40 +0200

    LSP support.

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    config = function()
+      local lsp_zero = require('lsp-zero')
+
+      lsp_zero.on_attach(function(client, bufnr)
+        lsp_zero.default_keymaps({buffer = bufnr})
+      end)
+      require('lspconfig').clangd.setup({})
+    end,
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-nvim-lsp',
+      'L3MON4D3/LuaSnip',
+    },
+  },
+}
